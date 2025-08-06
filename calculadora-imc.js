66579fccadbd2afe433138a9a4e0a9895e38d3ed
GuilhermeSMotta <guilhermesm221@outlook.com>
2025-08-06 20:21:24 -0300

    Commit 2

--- calculadora-imc.js
+++ calculadora-imc.js
@@ -1,7 +1,7 @@
 const altura = 1.72
 const peso = 60
 
-if (altura > 0 && peso > 0) {
+if (altura > 0 && altura < 3 && peso > 0) {
     const imc = peso / (altura * altura)
     console.log("IMC: " + imc)
 }
